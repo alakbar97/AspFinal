33e0daa7b7a71952be99fde6dcc2d696e287a186
alakbar97 <alakbarsa@code.edu.az>
2019-05-30 00:09:41 +0400

    reload

--- OnlineCvSolution/OnlineCv.WebUI/Models/Entity/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿using Vizew.WebUI.Models.Entity;
-
-namespace OnlineCv.WebUI.Models.Entity
-{
-    public class Category:BaseEntity
-    {
-        public string Name { get; set; }
-    }
-}
--- OnlineCvSolution/OnlineCv.WebUI/Models/Entity/Skill.cs
+++
@@ -1,9 +0,0 @@
-﻿using Vizew.WebUI.Models.Entity;
-
-namespace OnlineCv.WebUI.Models.Entity
-{
-    public class Skill:BaseEntity
-    {
-        public string Name { get; set; }
-    }
-}
